b01cb55d9e944e75fbfb051d26489963a08b1acd
SIVARAM.P <148986922+GEGAWON@users.noreply.github.com>
2026-01-13 01:12:37 +0530

    has error need to work on views form connection established

--- login_backend/accounts/views.py
+++ login_backend/accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 
 # Create your views here.
@@ -11,6 +12,21 @@ def logoutuserfun(request):
 def loginfun(request):
     return render(request,'login.html')
 def signupfun(request):
+    if request.method == 'POST':
+        username = request.POST.get('userid')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        fullname = request.POST.get('fullname')
+        email = request.POST.get('email')
+        profilepic = request.FILES.get('profilepic')
+        phone = request.POST.get('phone')
+        about = request.POST.get('about')
+        if not all([fullname, email, username, password, password2]):
+            messages.error(request, "didnt recieve all fields")
+            return render(request, 'signup.html')
+        else:
+            messages.success(request, "all fields received")
+            return render(request, 'signup.html')
     return render(request,'signup.html')
 
 def dashboardfun(request):
