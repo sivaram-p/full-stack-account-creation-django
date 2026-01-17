8aefd97ef04eb7a9a04ce86b3913c16a319908c0
SIVARAM.P <148986922+GEGAWON@users.noreply.github.com>
2026-01-18 00:51:20 +0530

    started fetching data from frontend using POST method ,also some filtration codes that can be used later after user creation part is done

--- login_backend/accounts/views.py
+++ login_backend/accounts/views.py
@@ -3,7 +3,10 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-
+try:
+    from .models import addons
+except ImportError:
+    addons = None 
 
 # Create your views here.
 def logoutuserfun(request):
@@ -14,19 +17,33 @@ def loginfun(request):
 def signupfun(request):
     if request.method == 'POST':
         username = request.POST.get('userid')
-        password = request.POST.get('password')
+        password1 = request.POST.get('password1')
         password2 = request.POST.get('password2')
         fullname = request.POST.get('fullname')
         email = request.POST.get('email')
         profilepic = request.FILES.get('profilepic')
         phone = request.POST.get('phone')
         about = request.POST.get('about')
-        if not all([fullname, email, username, password, password2]):
-            messages.error(request, "didnt recieve all fields")
+        if not all([fullname, email, username, phone, password1, password2]):
+            messages.error(request, "400 Bad Request - Didnt recieve all fields")
             return render(request, 'signup.html')
-        else:
-            messages.success(request, "all fields received")
+        
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "HTTP 409 Conflict - Email already registered.")
             return render(request, 'signup.html')
+        
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "HTTP 409 Conflict - Username already taken.")
+            return render(request, 'signup.html')
+
+        if password1 != password2:
+            messages.error(request, "Passwords do not match.")
+            return render(request, 'signup.html')
+        #if addons.phone.filter(phone=phone).exists():
+        #    messages.error(request, "Phone number already registered.")
+         #   return render(request, 'signup.html')
+
+        
     return render(request,'signup.html')
 
 def dashboardfun(request):
